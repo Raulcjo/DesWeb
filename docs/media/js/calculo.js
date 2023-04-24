0acda923d7a4312d63f6193874edeb4f8569e7b4
Raul A. Costa <113145571+Raulcjo@users.noreply.github.com>
2023-04-23 22:55:35 -0300

    Update calculo.js

--- docs/media/js/calculo.js
+++ docs/media/js/calculo.js
@@ -3,6 +3,7 @@ function getNotas() {
     let nota2 = parseInt(document.getElementById("nota2").value);
     let nota3 = parseInt(document.getElementById("nota3").value);
     let nota4 = parseInt(document.getElementById("nota4").value);
+    let nome = document.getElementById("nome").value;
 
     let media = (nota1 + nota2 + nota3 + nota4)/4;
     console.log("A média é " + media);
@@ -10,17 +11,17 @@ function getNotas() {
     let resultado = document.getElementById("resultado");
 
     if(media > 6.0){
-        resultado.innerHTML = `<p>Aluno aprovado, média é ${media}</p>`;
+        resultado.innerHTML = `<p>${nome} está aprovado(a), média é ${media}</p>`;
         document.getElementById("resultado").style.backgroundColor = 'lightgreen'; 
         console.log("Aluno foi aprovado");
     }
     else if(media < 2.0){
-        resultado.innerHTML = `<p>Aluno reprovado, média é ${media}</p>`;
+        resultado.innerHTML = `<p>${nome} está reprovado(a), média é ${media}</p>`;
         document.getElementById("resultado").style.backgroundColor = 'lightsalmon';
         console.log("Aluno foi reprovado");
     }
     else if(media > 2.1 && media < 5.9){
-        resultado.innerHTML = `<p>Aluno está de recuperação, média é ${media}</p>`;
+        resultado.innerHTML = `<p>${nome} está de recuperação, média é ${media}</p>`;
         document.getElementById("resultado").style.backgroundColor = 'lightblue';
         console.log("Aluno está de recuperação");
     }
