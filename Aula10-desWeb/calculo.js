f0007b74908a983fced9fedfc0597f9029aedd86
Raul A. Costa <113145571+Raulcjo@users.noreply.github.com>
2023-05-03 21:24:13 -0300

    add: Aula 10

---
+++ Aula10-desWeb/calculo.js
@@ -0,0 +1,11 @@
+function Salvar(){
+    let tableUser = document.querySelector('table');
+
+    let nome = document.getElementById('nome');
+    let email = document.getElementById('email');
+    let nascimento = document.getElementById('nasc');
+    let uf = document.querySelector("#uf");
+
+    
+
+}
