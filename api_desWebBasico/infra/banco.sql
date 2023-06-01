9bf32da8a8215294d540ae8e2e9484d580bf654c
Raul A. Costa <113145571+Raulcjo@users.noreply.github.com>
2023-05-31 22:16:29 -0300

    add: Aula 14

---
+++ api_desWebBasico/infra/banco.sql
@@ -0,0 +1,12 @@
+CREATE SCHEMA apidevweb;
+USE apidevweb;
+
+---Inicie o Xampp
+---Execute o MySQL no XAMPP
+---No workbench acesse a home (casinha no canto superiror esquerdo)
+---Clique no símbulo de +
+---Nome do Conexão XAMPP
+---host: localhost
+---user: rootn
+---senha: vazio
+---Porta: 3307
